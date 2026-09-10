e4a4120a6f438d0105a38ab72c93b18443831ec0
gilvan <gilvananjos10ns@gmail.com>
2026-09-10 17:05:48 -0300

    teste20 pipeline

--- deploy/scripts/before_install.sh
+++ deploy/scripts/before_install.sh
@@ -12,9 +12,7 @@ echo "Preparando diretório da aplicação..."
 
 mkdir -p "$APP_DIR"
 
-# Garante que o ec2-user seja o dono da pasta
-chown -R ec2-user:ec2-user "$APP_DIR"
-chmod -R 755 "$APP_DIR"
+
 
 echo "Diretório da aplicação:"
 ls -ld "$APP_DIR"
