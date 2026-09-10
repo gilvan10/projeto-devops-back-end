01bad7799409521782e02f172782df1c67dc07f0
gilvan <gilvananjos10ns@gmail.com>
2026-09-10 14:43:25 -0300

    teste17 pipeline

--- src/main/java/projeto/projeto_devops_back_end/controller/PrestadorServicoController.java
+++ src/main/java/projeto/projeto_devops_back_end/controller/PrestadorServicoController.java
@@ -26,7 +26,7 @@ public class PrestadorServicoController {
     //para a verificação de integridade do target group aws. aqui retorna o status code 200 + a string ok
     @GetMapping(value = "/check")
     public ResponseEntity<String> check() {
-        return ResponseEntity.ok("ok teste pipeline teste16");
+        return ResponseEntity.ok("ok teste pipeline teste17");
     }
     //ou bem simples
     //@GetMapping("/check")
