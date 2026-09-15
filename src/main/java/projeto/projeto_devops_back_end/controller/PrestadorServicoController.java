d294cff6ab2f4c613320a1d4ca6e657d8d55b756
gilvan <gilvananjos10ns@gmail.com>
2026-09-15 18:04:28 -0300

    teste45 pipeline

--- src/main/java/projeto/projeto_devops_back_end/controller/PrestadorServicoController.java
+++ src/main/java/projeto/projeto_devops_back_end/controller/PrestadorServicoController.java
@@ -32,7 +32,7 @@ public class PrestadorServicoController {
 
     @GetMapping(value = "/teste")
     public ResponseEntity<String> teste() {
-        return ResponseEntity.ok("ok pipeline teste43");
+        return ResponseEntity.ok("ok pipeline teste45");
     }
 
     //ou bem simples
